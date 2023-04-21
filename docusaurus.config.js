6b7f869f8bc988b6b058c7c819c1d65ab27fdabf
devbewill <sedprc@gmail.com>
2023-04-21 15:42:11 +0200

    added ignore to onBrokenLinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: "Mangrovia", // Usually your GitHub org/user name.
   projectName: "Datome", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
